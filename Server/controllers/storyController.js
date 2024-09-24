1b072c0d440d569ca045afc9a52d9ac981325245
ImKetan1610 <ketan.makode@gmail.com>
2024-09-25 00:20:54 +0530

    added routes in the storyRoutes

---
+++ Server/controllers/storyController.js
@@ -0,0 +1,18 @@
+// create the story
+const createStory = () => {};
+// get all the story
+const getAllStories = () => {};
+// get story by id
+const getStoryById = () => {};
+// update the story
+const updateStory = () => {};
+// delete the story
+const deleteStory = () => {};
+
+module.exports = {
+  createStory,
+  getAllStories,
+  getStoryById,
+  updateStory,
+  deleteStory,
+};
--- Server/routes/storyRoutes.js
+++ Server/routes/storyRoutes.js
@@ -1,5 +1,27 @@
-const express = require ('express');
+const express = require("express");
+const { protectedRoute } = require("../middleware/authMiddleware");
+const {
+  createStory,
+  getAllStories,
+  getStoryById,
+  updateStory,
+  deleteStory,
+} = require("../controllers/storyController");
 const router = express.Router();
 
+// create new story (protected route)
+router.post("/create", protectedRoute, createStory);
+
+// get all story
+router.get("/", getAllStories);
+
+// get story by id
+router.get("/:id", getStoryById);
+
+// update the specific story (protected route)
+router.put("/update/:id", protectedRoute, updateStory);
+
+// delete the specific story (protected route)
+router.delete("/delete/:id", protectedRoute, deleteStory);
 
 module.exports = router;
