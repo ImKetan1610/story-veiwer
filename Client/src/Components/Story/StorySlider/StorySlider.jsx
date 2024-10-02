7bc94f559f584d2df76f32c13694db8c3abee7f4
ImKetan1610 <ketan.makode@gmail.com>
2024-10-02 18:00:52 +0530

    added frontend

--- Client/src/Components/Story/StorySlider/StorySlider.jsx
+++ Client/src/Components/Story/StorySlider/StorySlider.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import Progress from "./Progress.jsx";
 import Slide from "./Slide.jsx";
 import { useSelector } from "react-redux";
